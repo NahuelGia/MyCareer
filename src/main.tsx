576169a003f305e9b2b7eecbd0b1f7e8e4b79c2c
NahuelGia <nahuelgiamminelli@gmail.com>
2025-04-21 10:21:44 -0300

    disabled themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { ThemeProvider } from "next-themes";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -10,15 +9,13 @@ import "./app.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
       <ChakraProvider value={defaultSystem}>
-         <ThemeProvider attribute="class" disableTransitionOnChange>
-            <BrowserRouter>
-               <Routes>
-                  <Route path="/" element={<MateriasPage />} />
-                  {/* <Route path="/" element={<App />} /> */}
-                  {/* Cuando tengamos la pantalla de inicio Materias debe estar en /materias */}
-               </Routes>
-            </BrowserRouter>
-         </ThemeProvider>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/" element={<MateriasPage />} />
+               {/* <Route path="/" element={<App />} /> */}
+               {/* Cuando tengamos la pantalla de inicio Materias debe estar en /materias */}
+            </Routes>
+         </BrowserRouter>
       </ChakraProvider>
    </React.StrictMode>
 );
