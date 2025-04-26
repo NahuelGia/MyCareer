a4674efd34faeaba2cd5e27dd058e8751459d9b7
ezegonzalez912 <ezegonzalez912@gmail.com>
2025-04-26 15:08:53 -0300

    add toast

--- src/hooks/useSubjectsActions.ts
+++ src/hooks/useSubjectsActions.ts
@@ -1,5 +1,6 @@
 import { useSubjects } from '../context/SubjectsContext';
 import { SubjectsStorageService } from '../services/storage/local-storage';
+import { toaster } from '@components/ui/toaster';
 
 export function useSubjectsActions() {
   const { setSubjectsData, setError } = useSubjects();
@@ -8,8 +9,28 @@ export function useSubjectsActions() {
     try {
       const updatedData = await SubjectsStorageService.updateSubjectStatus(subjectId, newStatus);
       setSubjectsData(updatedData);
+      
+      toaster.create({
+        title: 'Se guardo el progreso.',
+        description: `La materia ha sido marcada como ${newStatus.toLowerCase()}.`,
+        type: 'info',
+        duration: 6000,
+        meta: {
+          closable: true,
+        },
+      });
     } catch (err) {
       setError(err instanceof Error ? err : new Error('Error updating status'));
+      
+      toaster.create({
+        title: 'Error',
+        description: 'No se pudo actualizar el estado de la materia',
+        type: 'error',
+        duration: 3000,
+        meta: {
+          closable: true,
+        },
+      });
     }
   };
 
