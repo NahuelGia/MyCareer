f37d3df1afe6ffb1a3fab275235935e46887d078
ezegonzalez912 <ezegonzalez912@gmail.com>
2025-04-28 14:44:03 -0300

    gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, Route, Router, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import { MateriasPage } from "./pages/materias/MateriasPage";
 import { SubjectsProvider } from "./context/SubjectsContext";
 import "./app.css";
@@ -11,13 +11,13 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
    <React.StrictMode>
       <ChakraProvider value={defaultSystem}>
          <SubjectsProvider>
-            <BrowserRouter>
+            <HashRouter>
                <Routes>
                   <Route path="/" element={<MateriasPage />} />
                   {/* <Route path="/" element={<App />} /> */}
                   {/* Cuando tengamos la pantalla de inicio Materias debe estar en /materias */}
                </Routes>
-            </BrowserRouter>
+            </HashRouter>
          </SubjectsProvider>
       </ChakraProvider>
    </React.StrictMode>
